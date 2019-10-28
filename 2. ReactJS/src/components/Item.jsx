18ccab366b27aa9358e7fbe1286ecc93900628fe
Alejandro <alex.otero-94@hotmail.com>
2019-10-28 13:23:21 +0100

    Añadido state

--- 2. ReactJS/src/components/Item.jsx
+++ 2. ReactJS/src/components/Item.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Item = props => (
-  <li>
-    {props.title}
-  </li>
-);
+class Item extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      completada: true
+    };
+  }
+
+  onClick = () => {
+    const { completada } = this.state;
+    this.setState({ completada: !completada });
+  };
+
+  render() {
+    const { completada } = this.state;
+    const { title } = this.props;
+
+    return (
+      <li>
+        {
+          completada ? (
+            <strike>{title}</strike>
+          ) : (
+            <>{title}</>
+          )
+        }
+        <input type="checkbox" checked={completada} onChange={this.onClick}/>
+      </li>
+    );
+  }
+}
 
 export default Item;
