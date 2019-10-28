baf476d132d4d0386aa0a49013be829761150345
Alejandro <alex.otero-94@hotmail.com>
2019-10-28 13:54:14 +0100

    Añadidos datos como array

--- 2. ReactJS/src/App.js
+++ 2. ReactJS/src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import Listado from "./components/Listado.jsx";
 import Item from "./components/Item.jsx";
 
+const datos = [
+  { id: 1, title: "Ir a la compra" },
+  { id: 2, title: "Aprender React" },
+  { id: 3, title: "Poner la lavadora" },
+  { id: 4, title: "Poner la lavadora" }
+];
+
 const App = () => {
   return (
     <>
       <h1>Tareas</h1>
       <Listado>
-        <Item title="Ir a la compra"/>
-        <Item title="Aprender React"/>
-        <Item title="Poner la lavadora"/>
+        {datos.map(({ title, id }) =>
+          <Item title={title} key={id}/>
+        )}
       </Listado>
     </>
   );
